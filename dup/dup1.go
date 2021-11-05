c16b3c83061488683ddd13bf2075e4151442392f
Peter Walker <petersinclairwalker@gmail.com>
2021-11-05 09:44:38 -0400

    first dup.go with test file

---
+++ dup/dup1.go
@@ -0,0 +1,25 @@
+// prints the text of each line that appears more than once
+// in the standard input, preceded by its count
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "os"
+)
+
+func main(){
+  counts := make(map[string]int)
+  input := bufio.NewScanner(os.Stdin) //input is a scanner type?
+  for input.Scan(){
+    counts[input.Text()]++
+  }
+  // ignoring errors from input.Err()
+  for line, n := range counts {
+    if n > 1 {
+      fmt.Printf("%d\t%s\n", n, line)
+    }
+  }
+
+
+}
