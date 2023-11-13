a71ef2084a63c37f5d4306b80d2ba117cb0dcd89
PetersWalker <petersinclairwalker@gmail.com>
2023-11-12 19:46:28 -0500

    ex 1.9 modified fetch

---
+++ ch1/1.8/fetch.go
@@ -0,0 +1,33 @@
+// Exercis e 1.8: Mo dif y fetch to add the prefix http:// to each argument URL if it is missing.
+// Yo u mig ht want to use strings.HasPrefix.
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") || !strings.HasPrefix(url, "https://") {
+			url = "http://" + url
+		}
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
